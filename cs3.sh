12632cc6d4681a551f919b14d02af94902db41d0
sajanchhetri <53268120+sajanchhetri@users.noreply.github.com>
2023-04-19 17:23:50 +0545

    Update cs3.sh

--- cs3.sh
+++ cs3.sh
@@ -9,11 +9,11 @@ case $choice in
   1) echo "Backing up files..."
      cd /sdcard/android/data # add your backup commands here
      zip -r cs3-backup.zip com.astragon.cs3
-     mv cs3-backup.zip /sdcard
+     mv cs3-backup.zip /~/crazy
      ;;
   2) echo "Restoring files..."
     rm -rf /sdcard/android/data/com.astragon.cs3
-     cd /sdcard # add your restore commands here
+     cd /~/crazy # add your restore commands here
     cp cs3-backup.zip /sdcard/android/data
      cd /sdcard/android/data
     unzip cs3-backup.zip
